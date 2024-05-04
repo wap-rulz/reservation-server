ed5e60a27062d724256bf3c22e67049a5b2e941f
adeesha <wap.rulz@gmail.com>
2024-05-04 23:07:24 +0530

    Logger related changes

--- src/main/java/ds/adeesha/cw2/services/GetItemServiceImpl.java
+++ src/main/java/ds/adeesha/cw2/services/GetItemServiceImpl.java
@@ -6,14 +6,10 @@ import ds.adeesha.cw2.grpc.GetItemResponse;
 import ds.adeesha.cw2.grpc.GetItemServiceGrpc;
 import ds.adeesha.cw2.grpc.Item;
 import io.grpc.stub.StreamObserver;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public class GetItemServiceImpl extends GetItemServiceGrpc.GetItemServiceImplBase {
-    private static final Logger logger = LogManager.getLogger(GetItemServiceImpl.class);
-
     private final ReservationServer server;
 
     public GetItemServiceImpl(ReservationServer server) {
@@ -22,16 +18,16 @@ public class GetItemServiceImpl extends GetItemServiceGrpc.GetItemServiceImplBas
 
     @Override
     public void getItems(GetItemRequest request, StreamObserver<GetItemResponse> responseObserver) {
-        logger.info("Request received");
+        System.out.println("Request received");
         List<Item> items = getItems();
         GetItemResponse response = GetItemResponse.newBuilder().addAllItems(items).build();
-        logger.info("Sending response with {} items", items.size());
+        System.out.println("Sending response with " + items.size() + " items");
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
 
     private List<Item> getItems() {
-        logger.info("Get all items");
+        System.out.println("Get all items");
         return server.getItems();
     }
 }
